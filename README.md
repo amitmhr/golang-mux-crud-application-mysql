3193fc7c5c566f77c8791bfef6e4e5f94f99a25b
amit <amit.mehra244@gmail.com>
2024-06-28 13:23:47 +0530

    db connection

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ run this command to install the mux package
 go get -u github.com/gorilla/mux
 create new router
 create function for handle the api request
+
+# Database connection steps
+
+go get -u github.com/go-sql-driver/mysql
--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
+// DB connection
+
+const (
+	dbname = "go-crud"
+)
+
+var db *sql.DB
+
+func init() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/go-crud")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	fmt.Println("Success!")
+}
 func main() {
 
 	// Create new route
@@ -19,9 +37,10 @@ func main() {
 	})
 	// endpoints, handler functions and HTTP method
 	router.HandleFunc("/health-check", HealthCheck).Methods("GET")
+	router.HandleFunc("/ping-db", dbPing).Methods("GET")
 
-	fmt.Println("server is up and running on port 80....")
-	log.Fatal(http.ListenAndServe(":80", router))
+	fmt.Println("server is up and running on port 7777....")
+	log.Fatal(http.ListenAndServe(":7777", router))
 
 }
 
@@ -31,4 +50,9 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 
 	//update response writer
 	fmt.Fprintf(w, "API is up and running")
+}
+
+func dbPing(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("connecte1")
+
 }
